1d3f09391fb2429d2a8c95093d9dc518c160b047
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2020-12-02 15:25:59 +0100

    Move fn's outside of struct (day 2)

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -11,10 +11,36 @@ fn main() {
         .lines()
         .map(|line| PasswordPolicy::new(line))
         .collect();
-    let valid_password_count = policies.iter().filter(|p| p.is_valid()).count();
-    let valid_password_count2 = policies.iter().filter(|p| p.is_valid2()).count();
-    println!("Result of puzzle 1: {}", valid_password_count);
-    println!("Result of puzzle 2: {}", valid_password_count2);
+
+    println!(
+        "Result of puzzle 1: {}",
+        policies
+            .iter()
+            .filter(|p| is_valid_based_on_letter_count(*p))
+            .count()
+    );
+    println!(
+        "Result of puzzle 2: {}",
+        policies
+            .iter()
+            .filter(|p| is_valid_based_on_letter_positions(*p))
+            .count()
+    );
+}
+
+fn is_valid_based_on_letter_count(policy: &PasswordPolicy) -> bool {
+    let s: String = policy
+        .password
+        .chars()
+        .filter(|c| *c == policy.letter)
+        .collect();
+
+    s.len() >= policy.low && s.len() <= policy.high
+}
+
+fn is_valid_based_on_letter_positions(policy: &PasswordPolicy) -> bool {
+    (policy.password.chars().nth(policy.low - 1).unwrap() == policy.letter)
+        ^ (policy.password.chars().nth(policy.high - 1).unwrap() == policy.letter)
 }
 
 #[derive(Debug)]
@@ -34,20 +60,5 @@ impl PasswordPolicy {
             letter: v[2].chars().next().unwrap(),
             password: v[3].to_string(),
         }
-    }
-
-    fn is_valid(&self) -> bool {
-        let s: String = self
-            .password
-            .chars()
-            .filter(|c| *c == self.letter)
-            .collect();
-
-        s.len() >= self.low && s.len() <= self.high
-    }
-
-    fn is_valid2(&self) -> bool {
-        (self.password.chars().nth(self.low - 1).unwrap() == self.letter)
-            ^ (self.password.chars().nth(self.high - 1).unwrap() == self.letter)
     }
 }
