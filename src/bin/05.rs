1f9dd13050d49e2c07ba75955bf8f13842dfe8ef
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2020-12-05 15:37:29 +0100

    Add solution for puzzle 2 of day 5

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -9,7 +9,31 @@ fn main() {
 
     let lines: Vec<&str> = file_content.lines().collect();
 
-    let mut highest_seat_id = 0;
+    let mut seat_ids = get_seat_ids(lines);
+    seat_ids.sort_unstable();
+    let highest_seat_id = seat_ids[seat_ids.len() - 1];
+
+    println!("Result of puzzle 1: {}", highest_seat_id);
+    println!(
+        "Result of puzzle 2: {}",
+        find_free_seat_id(seat_ids).unwrap()
+    );
+}
+
+fn find_free_seat_id(seat_ids: Vec<u32>) -> Option<u32> {
+    let mut previous_id = 0;
+
+    for seat_id in seat_ids {
+        if (seat_id - previous_id) == 2 {
+            return Some(seat_id - 1);
+        }
+        previous_id = seat_id;
+    }
+    None
+}
+
+fn get_seat_ids(lines: Vec<&str>) -> Vec<u32> {
+    let mut seat_ids: Vec<u32> = Vec::new();
     for line in lines {
         let s: String = line
             .chars()
@@ -23,11 +47,7 @@ fn main() {
         let column = u32::from_str_radix(binary_column, 2).unwrap();
 
         let seat_id = 8 * row + column;
-
-        if seat_id > highest_seat_id {
-            highest_seat_id = seat_id;
-        }
+        seat_ids.push(seat_id);
     }
-
-    println!("Result of puzzle 1: {}", highest_seat_id);
+    seat_ids
 }
