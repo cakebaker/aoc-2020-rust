cbc2e345170ca558d8a88f32953a558d11939c76
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2020-12-22 13:45:40 +0100

    Add solution for puzzle 2 of day 22

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -9,12 +9,17 @@ fn main() {
         fs::read_to_string(filename).expect("Something went wrong reading the file!");
 
     let (cards_player_one, cards_player_two) = parse(file_content);
-    let score = play(cards_player_one, cards_player_two);
+    let mut winning_score = play_combat(cards_player_one.clone(), cards_player_two.clone());
 
-    println!("Result of puzzle 1: {}", score);
+    println!("Result of puzzle 1: {}", winning_score);
+
+    let (score_one, score_two) = play_recursive_combat(cards_player_one, cards_player_two);
+    winning_score = if score_one == 0 { score_two } else { score_one };
+
+    println!("Result of puzzle 2: {}", winning_score);
 }
 
-fn play(a: VecDeque<usize>, b: VecDeque<usize>) -> usize {
+fn play_combat(a: VecDeque<usize>, b: VecDeque<usize>) -> usize {
     let mut cards_player_one = a;
     let mut cards_player_two = b;
 
@@ -34,6 +39,57 @@ fn play(a: VecDeque<usize>, b: VecDeque<usize>) -> usize {
             break calculate_score(cards_player_two);
         } else if cards_player_two.is_empty() {
             break calculate_score(cards_player_one);
+        }
+    }
+}
+
+fn play_recursive_combat(a: VecDeque<usize>, b: VecDeque<usize>) -> (usize, usize) {
+    let mut cards_player_one = a;
+    let mut cards_player_two = b;
+    let mut history = Vec::new();
+
+    loop {
+        if history.contains(&cards_player_one) {
+            // player one wins
+            break (1, 0);
+        }
+
+        history.push(cards_player_one.clone());
+
+        let card_player_one = cards_player_one.pop_front().unwrap();
+        let card_player_two = cards_player_two.pop_front().unwrap();
+        let mut player_one_wins_round = false;
+
+        if cards_player_one.len() >= card_player_one && cards_player_two.len() >= card_player_two {
+            let (score_one, _) = play_recursive_combat(
+                cards_player_one
+                    .iter()
+                    .take(card_player_one)
+                    .cloned()
+                    .collect(),
+                cards_player_two
+                    .iter()
+                    .take(card_player_two)
+                    .cloned()
+                    .collect(),
+            );
+            player_one_wins_round = score_one > 0;
+        } else {
+            player_one_wins_round = card_player_one > card_player_two;
+        }
+
+        if player_one_wins_round {
+            cards_player_one.push_back(card_player_one);
+            cards_player_one.push_back(card_player_two);
+        } else {
+            cards_player_two.push_back(card_player_two);
+            cards_player_two.push_back(card_player_one);
+        }
+
+        if cards_player_one.is_empty() {
+            break (0, calculate_score(cards_player_two));
+        } else if cards_player_two.is_empty() {
+            break (calculate_score(cards_player_one), 0);
         }
     }
 }
