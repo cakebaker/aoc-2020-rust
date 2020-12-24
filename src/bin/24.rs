0fd8a8c6b2e3295f7b9c0e505f2f1e8dcc27a974
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2020-12-24 16:23:23 +0100

    Add (slow) solution for puzzle 2 of day 24

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,16 +1,24 @@
 use std::collections::HashSet;
 use std::env;
 use std::fs;
+use std::iter::FromIterator;
 
 fn main() {
+    const MAX_DAYS: usize = 100;
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let file_content =
         fs::read_to_string(filename).expect("Something went wrong reading the file!");
 
-    let black_tiles = get_black_tiles(parse(file_content));
+    let mut black_tiles = get_black_tiles(parse(file_content));
 
     println!("Result of puzzle 1: {}", black_tiles.len());
+
+    for _ in 1..=MAX_DAYS {
+        black_tiles = daily_flip(&black_tiles);
+    }
+
+    println!("Result of puzzle 2: {}", black_tiles.len());
 }
 
 fn get_black_tiles(tiles: Vec<Vec<Direction>>) -> HashSet<Position> {
@@ -58,6 +66,56 @@ fn identify_tile(directions: &[Direction]) -> Position {
     Position { x, y }
 }
 
+fn daily_flip(current_black_tiles: &HashSet<Position>) -> HashSet<Position> {
+    let mut next_black_tiles = HashSet::new();
+    let mut potential_black_tiles = HashSet::new();
+
+    for position in current_black_tiles {
+        let count = count_neighbors(&current_black_tiles, *position);
+
+        if count == 1 || count == 2 {
+            next_black_tiles.insert(*position);
+        }
+
+        potential_black_tiles.extend(HashSet::<Position>::from_iter(get_neighbor_positions(
+            *position,
+        )));
+    }
+
+    for position in potential_black_tiles {
+        if count_neighbors(current_black_tiles, position) == 2 {
+            next_black_tiles.insert(position);
+        }
+    }
+
+    next_black_tiles
+}
+
+fn count_neighbors(positions: &HashSet<Position>, position: Position) -> usize {
+    positions
+        .iter()
+        .filter(|&p| is_neighbor(*p, position))
+        .count()
+}
+
+fn get_neighbor_positions(position: Position) -> Vec<Position> {
+    let neighbor_positions = vec![(2, 0), (-2, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)];
+    neighbor_positions
+        .iter()
+        .map(|(x, y)| Position {
+            x: position.x + x,
+            y: position.y + y,
+        })
+        .collect()
+}
+
+fn is_neighbor(a: Position, b: Position) -> bool {
+    let neighbor_positions = vec![(2, 0), (-2, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)];
+    neighbor_positions
+        .iter()
+        .any(|(x, y)| a.x + x == b.x && a.y + y == b.y)
+}
+
 fn parse(file_content: String) -> Vec<Vec<Direction>> {
     let lines: Vec<&str> = file_content.lines().collect();
     let mut tiles = Vec::new();
@@ -99,7 +157,7 @@ fn parse(file_content: String) -> Vec<Vec<Direction>> {
     tiles
 }
 
-#[derive(Debug, Eq, PartialEq, Hash)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 struct Position {
     x: isize,
     y: isize,
